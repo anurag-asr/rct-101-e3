0101d48b7dd6ccd9d685b4f2b8d4140840ab1fe8
anurag-asr <anurjwt@gmail.com>
2022-06-06 23:35:59 +0530

    AD

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -1,7 +1,22 @@
-import React, { createContext } from "react";
+import React, { createContext, useEffect, useState } from "react";
+import axios from "axios";
 
 export const CartContext = createContext();
 
 export const CartProvider = ({ children }) => {
-  return <CartContext.Provider>{children}</CartContext.Provider>;
+  const [cartCount, setCartCount] = useState(0);
+
+  const increment = () => {
+    setCartCount(cartCount+1);
+  }
+
+  const decrement = () => {
+    setCartCount(cartCount-1);    
+  }
+
+  return (
+  <CartContext.Provider value={ {cartCount, increment, decrement} }>
+    {children}
+  </CartContext.Provider>  
+  );
 };
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,12 +1,19 @@
-import React, { useState, useContext } from "react";
+import React, { useContext } from "react";
 import { AuthContext } from "../context/AuthContext";
 
 const Login = () => {
+
+  const { login } = useContext(AuthContext);
+
   return (
     <div>
+       <h2>Login</h2>
       <input data-cy="login-email" />
+      <br />
       <input data-cy="login-password" />
-      <button data-cy="login-submit"></button>
+      <br />
+      <button data-cy="login-submit" onClick={login}>Login</button>
+      
     </div>
   );
 };
